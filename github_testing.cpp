bb66ee9a3055e7dea1ff8e7e8d6e688d7f0e5d10
yomesh <yomesh6118111@gmail.com>
2021-09-18 23:00:28 +0530

    new line added

---
+++ github_testing.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h> 
+
+#define ll long long
+#define ull unsigned long long
+#define vi  vector<int> 
+#define pb push_back
+#define F first
+#define S second
+#define MP Make_pair
+#define rep(i,a,b) for(int i = a; i<=b;i++
+using namespace std; 
+
+int main() 
+{
+    int i =0;
+    for(int j=0;i<10;j++)
+    {
+        cout<<j<<"\n";
+    }
+     return 0;
+}
