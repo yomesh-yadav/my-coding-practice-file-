2eeda8f398eb7f6ba8f79c52d4d9cf2e392771e2
yomesh <yomesh6118111@gmail.com>
2021-09-18 23:13:59 +0530

    first programme

---
+++ hello_world.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h> 
+
+#define ll long long
+#define ull unsigned long long
+#define vi  vector<int> 
+#define pb push_back
+#define F first
+#define S second
+#define MP Make_pair
+#define rep(i,a,b) for(int i = a; i<=b;i++
+using namespace std; 
+
+int main() 
+{
+    cout<<"hello world";
+     return 0;
+}
